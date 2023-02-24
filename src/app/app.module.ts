fe225108be4c86557f83b74b8c163bfeca314d61
nkunic <45877400+nkunic@users.noreply.github.com>
2023-02-24 16:02:19 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ToggleModule } from './toggle/toggle.module';
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, MaterialModule, ToggleModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ToggleModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
